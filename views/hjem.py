8fd8a4d79cad29c7f5cf4bbd5039d2a8443fc173
BendikB-J <79855318+BendikB-J@users.noreply.github.com>
2024-12-18 21:08:03 +0100

    Update hjem.py

--- views/hjem.py
+++ views/hjem.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
 
-st.image("selmaBursdag/images/images-2.png", width = 300)
+st.image("images/images-2.png", width = 300)
 
 
 if st.button("Den mystiske forsvinningen i Larvik"):
     st.switch_page("views/dokumenter.py")
 
-st.image("selmaBursdag/images/mannibutikk.jpg", width = 500)
+st.image("images/mannibutikk.jpg", width = 500)
 st.write("")
 
 st.title(" ")
 if st.button("Det forsvunnede maleri"):
     st.switch_page("views/dokumenter.py")
 
-st.image("selmaBursdag/images/Kunstbilde.jpg", width = 500)
+st.image("images/Kunstbilde.jpg", width = 500)
 
