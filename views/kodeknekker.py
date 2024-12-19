9c9f99538b510bc807e7b3c6c4b962ddc234e16c
BendikB-J <79855318+BendikB-J@users.noreply.github.com>
2024-12-19 08:07:09 +0100

    Update kodeknekker.py

--- views/kodeknekker.py
+++ views/kodeknekker.py
@@ -39,7 +39,7 @@ if st.session_state.input_locked:
                  disabled=True)
     countdown_timer()
 else:
-    user_input = st.text_input("Skriv inn riktig kode for å låse opp flere sider:")
+    user_input = st.text_input("Skriv inn riktig kode for å låse opp flere sider:", type = "default")
 
     if user_input:
         if user_input == "Rødborge":
